475b14afc6c961a138dbbb7baf8434fddd2b33d4
RaulCruzRamirez <71723606+RaulCruzRamirez@users.noreply.github.com>
2025-12-04 02:16:34 +0000

    Beta 01

--- webFunctions.hpp
+++ webFunctions.hpp
@@ -8,26 +8,34 @@ using namespace std;
 #include <filesystem>
 namespace fs = std::filesystem;
 
-//template <typename Information> class webBrowser {
-//private:
+//The main registry is a multimap, with the key being the word and the value being that of a pair which
+//contains a string, for the name of the file, and an interger, for the number of times the word is present.
 multimap<string, pair<string,int>> fullDictionary;
-//public:
-string cleanupWord(string& word){//const{
+//This function serves to clean up the words, those recieved from the files in the directory and inputed by the user.
+//It cleans up both at the beginning and the end of the string, removing commas, parenthesis, exclamation and question
+//marks, among other characters that interfere with the recognition of the word, it also puts the word in lowercase.
+//It requires a word, string, to function.
+string cleanupWord(string& word){
     set<char> lastSpace = {',','.',';',':','"',']','}',')','>','%','!','?','/','_','-','+','=','|','\''};
     set<char> firstSpace = {'{','[','(','<','/','"','-','+','=','|','\''};
     string fullWord = word;
-
-    if(lastSpace.count(fullWord[fullWord.length() - 1]) == true){fullWord.pop_back();}
     if(lastSpace.count(fullWord[fullWord.length() - 2]) == true){fullWord.pop_back();fullWord.pop_back();}
+    if(lastSpace.count(fullWord[fullWord.length() - 1]) == true){fullWord.pop_back();}
     if(firstSpace.count(fullWord[0]) == true){fullWord.erase(0,1);}
-
     for (auto& letterS : fullWord) {
         letterS = tolower(letterS);
     }
-
     return fullWord;
 }
 
+//The main function of the program; It takes the word that is inputed by the current string in the file
+//it takes the key range, it contains all the keys that match with the current word. First it checks if the word 
+//is in the multimap, if it is not the it inserts the word into the keys in the multimap, while also inserting
+//the pair of the current file and a 1 for it being the first time it shows up. If the word is in the multimap
+//it then checks to see if the file has already been added to the registry, if it has been added it then searches
+//to see if any of the current pairs contains the current file, if it does then it updates the count adding a 1 to
+//the total, instead if the file is not found then it inserts the pair, file name and 1 count, to the multimap.
+//It requires a fileTitle and word in order to function, both in string.
 void catalogueWord(string &fileTitle, string fileWord){
     auto wordInDictionary = fullDictionary.find(fileWord);
     bool updatedCount = false;
@@ -44,6 +52,12 @@ void catalogueWord(string &fileTitle, string fileWord){
     }
 }
 
+//Here is where all the files in the directory are read and the words inside are registered into the registry.
+//It opens the current file, making sure it works, then it updates a counter of the total amount of files searched,
+//before starting the looking at the file string by string it isolates the name of the current file in a variable.
+//It then goes string by string, calling the catalogueWord function to register it giving the function the file name
+//and invoking the cleanupWord function to clean the word before it gets sent to the previous function. It does this
+//for every file and every word inside it. It requires a directory, in string form.
 int readFiles(const string& directory) {
   int counter = 0;
   string fileWord;
--- webMain.cpp
+++ webMain.cpp
@@ -11,14 +11,26 @@ using namespace std;
 namespace fs = std::filesystem;
 
 int main() {
-  string fileName, fileWord;
-  int totalFiles = readFiles("moviesdb2");
+  string fileName, fileWord, filesDirectory;
+  //Using filesystem it iterates over all the files in the directory, inside the same function is
+  //where the registry is built for each word encountered in the files, it asks the user as to which
+  //directory it will traverse. It requires the directory, string, to function.
+  cout << "Input The File Directory: ";
+  cin >> filesDirectory;
+  int totalFiles = readFiles(filesDirectory);
 
+  //The user interface of the browser, this is where the user chooses between searching one or two words
+  //and exiting the browser. Its divided in two parts one for the single word search: this one iterates over
+  //all the files containing the selected key word, searching for the ones with the most occurences then it
+  //puts them into three pairs for 1st,2nd,3rd place, shifting the places as new files are looked at that have
+  //higher occurences than others. For the two word search it   FINISH HERE .
   short browserChoose = 0;
   string searchWord, userWord0, userWord1, userWord2;
-      
+  cout << "There is a total of " << totalFiles << " files in the directory." << endl
+  << "To choose an option enter the corresponding number." << endl;
   while (browserChoose!=-1){
-    cout << "Search Functions" << endl <<
+    cout << "====================================================================================" << endl;
+    cout << "Search Functions: " <<
     "Search One Word (1) -" << "- Search Two Word (2) -" <<
     "- Exit Browser (-1)" << endl << "Select Function: ";
     cin >> browserChoose;
@@ -61,17 +73,17 @@ int main() {
       "3. File Name: " << thirdPlace.second << " - Times Found: " << thirdPlace.first << endl;
     }
     if(browserChoose==2){
+      //Double Word Search
       cout << "Search First Word: ";
       cin >> userWord1;
       searchWord = cleanupWord(userWord1);
       cout << "Search Second Word: ";
       cin >> userWord2;
       searchWord = cleanupWord(userWord2);
-
-
+      //MAKE THE TWO WORD SEARCH
     }
   }
-
-  cout << "Browser Exited";
+  //The end of the function as it exits the progam when the user has finished browsing.
+  cout << "Browser Exited" << endl;
   return 0;
 }
